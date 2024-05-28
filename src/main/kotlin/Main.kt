848ec859f8e1a5a44383871fb560801bd3bf9353
Joel Eriksson Sinclair <joeles76@gmail.com>
2024-05-28 09:57:09 +0200

    Basic UI and flow control to interface with the repositories.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
-fun main(args : Array<String>) {
+import data.repository.InventoryRepository
+import data.repository.InventoryRepositoryRAM
+import data.repository.ProductRepository
+import data.repository.ProductRepositoryRAM
+import data.view.ConsoleView
+
+fun main() {
     println("Running IKEA Warehouse...")
+
+    val inventoryRepo : InventoryRepository = InventoryRepositoryRAM()
+    val productRepo : ProductRepository = ProductRepositoryRAM(inventoryRepo)
+
+    ConsoleView(productRepo, inventoryRepo).showMainMenu()
 }
